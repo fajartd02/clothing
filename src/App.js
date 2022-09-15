58a8c0c5b9e550225836c7c0bc39dcedbaf6c2b8
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-09-15 19:34:25 +0800

    add category body className

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ const App = () => {
   return (
     <div className="categories-container">
       <div className="category-container">
-        <h1>Hats</h1>
-        <p>Shop Now</p>
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h1>Hats</h1>
+          <p>Shop Now</p>
+        </div>
       </div>
     </div>
   );
