256ce170e707353c514fb173823413bf83ebfd5f
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-09-18 17:42:26 +0800

    add outlet router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Home from "./routes/home/home.component";
 import { Route, Routes } from "react-router-dom";
 
+const Shop = () => {
+  return <h1>This is Shop !</h1>;
+};
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />}>
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
