9a4132ee19a04eb1061e1eb11c4557f193f35f36
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2022-09-28 01:09:32 +0800

    routes dynamic category

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,20 +1,16 @@
-import { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../category-preview/category-preview.component";
 
-import CategoryPreview from "../../components/category-preview/category-preview.component";
-import { CategoriesContext } from "../../context/categories.context";
+import Category from "../category/category.component";
 
 import "./shop.styles.scss";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((key) => {
-        const products = categoriesMap[key];
-        return <CategoryPreview key={key} title={key} products={products} />;
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+    </Routes>
   );
 };
 
